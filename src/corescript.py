9bb0e0bf9ef7644df66948a1fbc1da9f9f2832d9
Chief-A <aathilr7@gmail.com>
2021-06-11 21:48:02 +0530

    Add corescript.py

---
+++ src/corescript.py
@@ -0,0 +1,6 @@
+#Whatsapp Automation
+#read input json file "input.json"
+#Load web.whatsapp.com and authenticate
+#wait till time reached
+#locate the chat, input text field and send the message
+#iterate through all messages
