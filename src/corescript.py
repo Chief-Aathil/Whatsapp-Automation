c9264d2f048b45b3d10244285c5a4bb4e9b8117c
Chief-A <aathilr7@gmail.com>
2021-06-14 19:54:10 +0530

    Implement basic structure

--- src/corescript.py
+++ src/corescript.py
@@ -1,6 +1,25 @@
 #Whatsapp Automation
-#read input json file "input.json"
-#Load web.whatsapp.com and authenticate
+
+
 #wait till time reached
 #locate the chat, input text field and send the message
 #iterate through all messages
+
+
+import json
+from selenium import webdriver
+
+#get msg list from "input.json"
+input_file="input.json"
+with open(input_file,'r',encoding='utf-8') as file:
+    input_list=json.load(file)
+    
+#Load web.whatsapp.com and authenticate
+driver = webdriver.Chrome("..\\chromedriver.exe")
+driver.get("https://web.whatsapp.com")
+
+
+
+for message_item in input_list:
+    #check_time(message_item)
+    # send_message(message_item)
