4b79602e9f0ca2de7469e5b849ef303944e66551
Chief-A <aathilr7@gmail.com>
2021-06-18 11:28:08 +0530

    Offset time in input.json from current time

--- src/testscript.py
+++ src/testscript.py
@@ -1,7 +1,8 @@
 import json
+from json import encoder
 import time
 import datetime
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 # print("Hello world")
@@ -15,25 +16,24 @@ list.append(y)
 # print(list)
 print(json.dumps(list, indent=4))
 
-file_name = 'test.json'
+file_name = 'test.json' #not using input.json, to do experiment
 
-# with open( file_name,'w', encoding='utf-8') as file:
-#     json.dump(list,file,ensure_ascii=False,indent=4)
-#     file.close()
+
 
 with open(file_name, 'r', encoding='utf-8') as file:
     output = json.load(file)
-    # json.dumps(output)
-    # print(json.dumps(output))
-    print(output)
     file.close()
 
-# output.append('2')
-# print('here')
-# print(json.dumps(output,indent=2))
-# print(output[1])
+for index,item in enumerate(output):
+    offset=timedelta(minutes=index+2) # offset = 2,3,4,...
+    # item[2]=datetime.strptime(item[2],"%Y-%m-%d %H:%M:%S") #to offset from the given time
+    item[2] = datetime.now()  # to offset from current time
+    item[2] = item[2]+offset  # add offset min
+    item[2] = datetime.strftime(item[2], "%Y-%m-%d %H:%M:%S")
 
-for item in output:
-    x=datetime.strptime(item[2],"%Y-%m-%d %H:%M:%S")
-    print(x)
-    print(x.month)
+print(output)
+
+with open('input.json','w',encoding='utf8',) as file_out:
+    json.dump(output,file_out,ensure_ascii=False, indent=4)
+    file_out.close()
+
