dfda64cd5ba31182255c8eeba959cdc8d8b9f67c
Chief-A <aathilr7@gmail.com>
2021-06-18 17:01:14 +0530

    Reduced offset time

--- src/testscript.py
+++ src/testscript.py
@@ -25,13 +25,14 @@ with open(file_name, 'r', encoding='utf-8') as file:
     file.close()
 
 for index,item in enumerate(output):
-    offset=timedelta(minutes=index+2) # offset = 2,3,4,...
+    offset=timedelta(minutes=index+1) # offset = 1,2,3,4,...
     # item[2]=datetime.strptime(item[2],"%Y-%m-%d %H:%M:%S") #to offset from the given time
     item[2] = datetime.now()  # to offset from current time
     item[2] = item[2]+offset  # add offset min
     item[2] = datetime.strftime(item[2], "%Y-%m-%d %H:%M:%S")
     item[1]= "msg @ "+item[2]
-print(output)
+
+json.dumps(output,indent=4)
 
 with open('input.json','w',encoding='utf8',) as file_out:
     json.dump(output,file_out,ensure_ascii=False, indent=4)
