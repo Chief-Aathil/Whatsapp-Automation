e4b1aed74c0a4e5fc46c9f340f8104287242e410
Chief-A <aathilr7@gmail.com>
2021-06-18 12:26:54 +0530

    Add time in message string Add the time of delivery in the message string. Used for debugging.

--- src/testscript.py
+++ src/testscript.py
@@ -7,14 +7,14 @@ from datetime import datetime, timedelta
 
 # print("Hello world")
 # time.sleep(3)
-print("Started")
-x = ['chat1', 'msg1', 't1']
-y = ['chat2', 'msg2', 't2']
-list = []
-list.append(x)
-list.append(y)
-# print(list)
-print(json.dumps(list, indent=4))
+# print("Started")
+# x = ['chat1', 'msg1', 't1']
+# y = ['chat2', 'msg2', 't2']
+# list = []
+# list.append(x)
+# list.append(y)
+# # print(list)
+# print(json.dumps(list, indent=4))
 
 file_name = 'test.json' #not using input.json, to do experiment
 
@@ -30,7 +30,7 @@ for index,item in enumerate(output):
     item[2] = datetime.now()  # to offset from current time
     item[2] = item[2]+offset  # add offset min
     item[2] = datetime.strftime(item[2], "%Y-%m-%d %H:%M:%S")
-
+    item[1]= "msg @ "+item[2]
 print(output)
 
 with open('input.json','w',encoding='utf8',) as file_out:
